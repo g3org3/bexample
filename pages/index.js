9eb1c9bf38f61dd371d4ce932e0f3c4d4aa87583
George <7jagjag@gmail.com>
2018-07-31 19:04:04 +0200

    add prettierrc for precommit script to be consistent with eslint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Head from "../components/head";
-import NeoChart from "../components/NeoChartLoader";
+import React from 'react';
+import Head from '../components/head';
+import NeoChart from '../components/NeoChartLoader';
 
 const Home = () => (
   <div
-    style={{ margin: "0px auto", maxWidth: "1000px", fontFamily: "helvetica" }}
+    style={{ margin: '0px auto', maxWidth: '1000px', fontFamily: 'helvetica' }}
   >
     <Head title="Home" />
     <h1>NASA Neo - Data Visualization</h1>
