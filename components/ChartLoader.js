acb41ec3852427d3e8d01a08af851e5ed5a2f7a3
George <7jagjag@gmail.com>
2018-07-31 22:30:47 +0200

    change how google graph is render by component

--- components/ChartLoader.js
+++ components/ChartLoader.js
@@ -2,11 +2,13 @@
 import React, { Component } from 'react';
 import Script from 'react-load-script';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 
 export default class ChartLoader extends Component {
   static propTypes = {
     titles: PropTypes.arrayOf(PropTypes.string),
     data: PropTypes.arrayOf(PropTypes.array),
+    options: PropTypes.object,
   };
 
   static defaultProps = {
@@ -16,6 +18,23 @@ export default class ChartLoader extends Component {
       'Max Estimated Diameter (km)',
     ],
     data: [['', 0, 0]],
+    options: {
+      width: 900,
+      chart: {
+        title: 'NASA NEO Data Visualization',
+      },
+      bars: 'horizontal',
+      series: {
+        0: { axis: 'min-distance' },
+        1: { axis: 'max-distance' },
+      },
+      axes: {
+        x: {
+          'min-distance': { label: 'Min Estimated Diameter (km)' },
+          'max-distance': { side: 'top', label: 'Max Estimated Diameter (km)' },
+        },
+      },
+    },
   };
   state = {
     loaded: false,
@@ -30,49 +49,43 @@ export default class ChartLoader extends Component {
   };
 
   chartLoaded = () => {
-    const { titles, data } = this.props;
     const chart = new google.charts.Bar(document.getElementById('dual_x_div'));
+    this.setState({ chart });
+  };
+
+  onError = () => {
+    toast('Could not load Google Charts :(', { type: 'warning' });
+    this.setState({ error: false });
+  };
+
+  static draw({ chart, titles, data, options }) {
     const dataTable = new google.visualization.arrayToDataTable([
       titles,
       ...data,
     ]);
-    chart.draw(dataTable, this.options);
-    this.setState({ chart });
-  };
+    chart.draw(dataTable, options);
+  }
 
-  options = {
-    width: 900,
-    chart: {
-      title: 'NASA NEO Data Visualization',
-    },
-    bars: 'horizontal',
-    series: {
-      0: { axis: 'min-distance' },
-      1: { axis: 'max-distance' },
-    },
-    axes: {
-      x: {
-        'min-distance': { label: 'Min Estimated Diameter (km)' },
-        'max-distance': { side: 'top', label: 'Max Estimated Diameter (km)' },
-      },
-    },
-  };
-
-  onError = () => {
-    this.setState({ error: false });
-  };
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.chart) {
+      const { titles, data, options } = nextProps;
+      const { chart } = prevState;
+      ChartLoader.draw({ chart, titles, data, options });
+    }
+    return nextProps;
+  }
 
   render() {
     if (this.state.error) {
       return (
         <div class="alert alert-warning" role="alert">
-          Sorry something went wrong. Try to refresh the page.
+          Sorry but the app fail to load Google Charts lib.
         </div>
       );
     }
     return (
       <div>
-        <div id="dual_x_div" style={{ width: '1000px', height: '800px' }} />
+        <div id="dual_x_div" style={{ width: '1000px', height: '700px' }} />
         <Script
           url="https://www.gstatic.com/charts/loader.js"
           onError={this.onError}
